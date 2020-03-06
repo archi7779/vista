9fb33717d060d10f4f98c98b8f222a6bb47471e8
archi777 <wowgladarena@mail.ru>
2020-03-06 19:06:03 +0300

    Updates

---
+++ precache-manifest.acfe79003bf9b8ad7789e42c1775a424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36de6ba68fc978937b92ee10c337d6b4",
+    "url": "/vista/index.html"
+  },
+  {
+    "revision": "4ad011ab14962d4fbf37",
+    "url": "/vista/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "4ad011ab14962d4fbf37",
+    "url": "/vista/static/js/2.9c67cbda.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/vista/static/js/2.9c67cbda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a1e462482490423fbe",
+    "url": "/vista/static/js/main.85f544cc.chunk.js"
+  },
+  {
+    "revision": "606d698537f2ad46fb11",
+    "url": "/vista/static/js/runtime-main.fdd49267.js"
+  }
+]);
